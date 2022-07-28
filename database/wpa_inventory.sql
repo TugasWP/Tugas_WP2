19e805aa5ce95d9e21bbf66805c51113eab8e904
ZyanL <zyanlilou@gmail.com>
2022-07-28 19:11:54 +0700

    update laporan harian bulanan

--- database/wpa_inventory.sql
+++ database/wpa_inventory.sql
@@ -27,12 +27,13 @@ CREATE TABLE IF NOT EXISTS `tbl_barang` (
   `updated_at` datetime DEFAULT '0000-00-00 00:00:00',
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id_barang`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
--- Dumping data for table wpa_inventory.tbl_barang: ~2 rows (approximately)
+-- Dumping data for table wpa_inventory.tbl_barang: ~3 rows (approximately)
 REPLACE INTO `tbl_barang` (`id_barang`, `kode_barang`, `nama_barang`, `harga_barang`, `satuan`, `id_operator`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, '001', 'Marlboro', 10000, 'Pcs', 1, '2022-06-15 19:05:54', '2022-06-15 19:05:54', '2022-06-22 00:08:07'),
-	(2, '002', 'LA', 12000, 'Pcs', 1, '2022-06-15 21:10:43', '2022-06-15 21:10:43', NULL);
+	(7, '001', 'boneka', 25000, 'Pcs', 1, '2022-07-27 18:34:57', '2022-07-27 18:34:57', NULL),
+	(8, '002', 'wafer', 15000, 'Pcs', 1, '2022-07-27 18:35:27', '2022-07-27 18:35:27', NULL),
+	(9, '003', 'penggaris', 5000, 'Pcs', 1, '2022-07-27 18:35:54', '2022-07-27 18:35:54', NULL);
 
 -- Dumping structure for table wpa_inventory.tbl_barang_keluar
 DROP TABLE IF EXISTS `tbl_barang_keluar`;
@@ -45,11 +46,15 @@ CREATE TABLE IF NOT EXISTS `tbl_barang_keluar` (
   `updated_at` datetime DEFAULT '0000-00-00 00:00:00',
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id_barang_keluar`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
--- Dumping data for table wpa_inventory.tbl_barang_keluar: ~1 rows (approximately)
+-- Dumping data for table wpa_inventory.tbl_barang_keluar: ~5 rows (approximately)
 REPLACE INTO `tbl_barang_keluar` (`id_barang_keluar`, `qty_keluar`, `id_barang`, `id_operator`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(4, 12, 1, 1, '2022-06-21 23:44:56', '2022-06-21 23:44:56', NULL);
+	(9, 50, 7, 1, '2022-07-27 18:37:57', '2022-07-27 18:37:57', NULL),
+	(10, 30, 8, 1, '2022-07-27 18:45:09', '2022-07-27 18:45:09', NULL),
+	(11, 23, 7, 1, '2022-07-27 18:51:45', '2022-07-27 18:51:45', NULL),
+	(12, 200, 9, 1, '2022-08-28 17:53:06', '2022-08-28 17:53:06', NULL),
+	(13, 50, 9, 1, '2022-09-28 19:03:13', '2022-09-28 19:03:13', NULL);
 
 -- Dumping structure for table wpa_inventory.tbl_barang_masuk
 DROP TABLE IF EXISTS `tbl_barang_masuk`;
@@ -62,12 +67,15 @@ CREATE TABLE IF NOT EXISTS `tbl_barang_masuk` (
   `updated_at` datetime DEFAULT '0000-00-00 00:00:00',
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id_barang_masuk`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Dumping data for table wpa_inventory.tbl_barang_masuk: ~2 rows (approximately)
+-- Dumping data for table wpa_inventory.tbl_barang_masuk: ~5 rows (approximately)
 REPLACE INTO `tbl_barang_masuk` (`id_barang_masuk`, `qty_masuk`, `id_barang`, `id_operator`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(4, 200, 2, 1, '2022-06-21 23:58:19', '2022-06-21 23:58:19', '2022-06-22 00:02:48'),
-	(5, 12, 2, 1, '2022-06-22 00:40:41', '2022-06-22 00:40:41', NULL);
+	(7, 100, 7, 1, '2022-07-27 18:37:04', '2022-07-27 18:37:04', NULL),
+	(8, 200, 8, 1, '2022-07-27 18:37:14', '2022-07-27 18:37:14', NULL),
+	(9, 50, 9, 1, '2022-07-27 18:37:26', '2022-07-27 18:37:26', NULL),
+	(10, 200, 9, 1, '2022-08-28 19:03:01', '2022-08-28 19:03:01', NULL),
+	(11, 30, 7, 1, '2022-09-28 19:03:42', '2022-09-28 19:03:42', NULL);
 
 -- Dumping structure for table wpa_inventory.tbl_operator
 DROP TABLE IF EXISTS `tbl_operator`;
@@ -81,11 +89,11 @@ CREATE TABLE IF NOT EXISTS `tbl_operator` (
   `updated_at` datetime DEFAULT '0000-00-00 00:00:00',
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id_operator`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Dumping data for table wpa_inventory.tbl_operator: ~2 rows (approximately)
+-- Dumping data for table wpa_inventory.tbl_operator: ~1 rows (approximately)
 REPLACE INTO `tbl_operator` (`id_operator`, `nama_operator`, `username`, `password`, `email`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'ucup', 'hlukma12', 'hellowq', 'surucup@gmail.com', '2022-06-16 15:19:07', '2022-06-16 15:19:07', NULL);
+	(1, 'ucup', 'ucup12', 'hellowq', 'surucup@gmail.com', '2022-06-16 15:19:07', '2022-06-16 15:19:07', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
